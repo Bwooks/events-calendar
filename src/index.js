8132178e1e6614d9bdb22dee601abefb38499da1
Brooks Dulla <brooksdulla@gmail.com>
2017-08-06 22:24:12 -0400

    Decided on application state shape and architecture

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
+import {Provider} from 'react-redux'
+import {configureStore} from './store/configureStore.js'
+import {initialState} from './store/initialState.js'
+import reducers from './store/rootReducer.js'
+import Calendar from './components/Calendar'
+import {initCalendar} from './models/Calendar'
+import moment from 'moment'
 
-
-
+const store = configureStore(reducers, initialState)
+store.dispatch(initCalendar())
 const rootComponent = (
-    <Provider>
-        <App />
+    <Provider store={store}>
+        <Calendar />
     </Provider>
-
-);
-ReactDOM.render(rootComponent, document.getElementById('root'));
-registerServiceWorker();
+)
+ReactDOM.render(rootComponent, document.getElementById('root'))
+registerServiceWorker()
